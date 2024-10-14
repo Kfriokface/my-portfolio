3dbfd9a952b6133e19d0d1d13c373c4880bc6bae
Alberto Sancho <designalbertosancho@gmail.com>
2024-10-15 00:37:33 +0200

    cambios

--- data/projects.js
+++ data/projects.js
@@ -3,7 +3,7 @@ export const projects = [
     title: "Webconsultas.com",
     description: "Revista digital de salud y bienestar",
     image:
-      "../public/images/webs/webconsultas.png",
+      "/images/webconsultas.png",
     tech: ["Drupal"],
     github: "",
     link: "https://www.webconsultas.com",
@@ -12,7 +12,7 @@ export const projects = [
     title: "Natalben.com",
     description: "Web para el producto farmacéutico Natalben",
     image:
-      "../public/images/webs/natalben.png",
+      "/images/natalben.png",
     tech: ["Drupal"],
     github: "",
     link: "https://www.natalben.com",
@@ -21,7 +21,7 @@ export const projects = [
     title: "Oniria",
     description: "Web para el producto farmacéutico Oniria",
     image:
-      "../public/images/webs/oniria.png",
+      "/images/oniria.png",
     tech: ["Wordpress"],
     github: "",
     link: "https://www.oniriabuenasnoches.com/",
@@ -30,7 +30,7 @@ export const projects = [
     title: "Zelesse",
     description: "Web para el producto farmacéutico Zelesse",
     image:
-      "../public/images/webs/zelesse.png",
+      "/images/zelesse.png",
     tech: ["Wordpress"],
     github: "",
     link: "https://www.zelesse.com/",
@@ -39,7 +39,7 @@ export const projects = [
     title: "Vanesa Fernández",
     description: "Web para la psicóloga Vanesa Fernández",
     image:
-      "../public/images/webs/vanesa_fernandez.png",
+      "/images/vanesa_fernandez.png",
     tech: ["Wordpress"],
     github: "http://github.com/myuser/proyecto4",
     link: "www.proyecto1.com",
@@ -48,7 +48,7 @@ export const projects = [
     title: "BodyEvolution",
     description: "Proyecto para la clínica de fisiorerapia Body Evolution",
     image:
-      "../public/images/webs/body_evolution.png",
+      "/images/body_evolution.png",
     tech: ["Wordpress"],
     github: "http://github.com/myuser/proyecto5",
     link: "https://bepilates.albertosancho.es/",
@@ -57,7 +57,7 @@ export const projects = [
     title: "AllFresco",
     description: "Proyecto para una tienda online de vegetales",
     image:
-      "../public/images/webs/allfresco.png",
+      "/images/allfresco.png",
     tech: ["Html5", "CSS3", "JavaScript"],
     github: "https://github.com/Kfriokface/mod_7_galeria_dinamica",
     link: "https://allfresco.netlify.app",
@@ -66,7 +66,7 @@ export const projects = [
     title: "Proyecto E-Commerce",
     description: "Proyecto para realizar un e-commerce de electrónica",
     image:
-      "../public/images/webs/myfirstprojectprometeo.png",
+      "/images/myfirstprojectprometeo.png",
     tech: ["Html5", "CSS3"],
     github: "https://github.com/Kfriokface/proyecto_1",
     link: "https://myfirstprojectprometeo.netlify.app",
@@ -75,7 +75,7 @@ export const projects = [
     title: "Alberto Sancho",
     description: "Proyecto personal para mostrar una galería de mis trabajos como diseñador gráfico",
     image:
-      "../public/images/webs/albertosanchoexample.png",
+      "/images/albertosanchoexample.png",
     tech: ["Html5", "CSS3"],
     github: "https://github.com/Kfriokface/personal_website",
     link: "https://albertosanchoexample.netlify.app",
