534771e8be128772217e19b221b0628cdebdb841
Alberto Sancho <designalbertosancho@gmail.com>
2024-10-22 23:33:14 +0200

    config

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -4,8 +4,8 @@ export async function handler(event) {
   const { email, message } = JSON.parse(event.body);
 
   const mg = mailgun({
-    apiKey: process.env.MAILGUN_API_KEY,
-    domain: process.env.MAILGUN_DOMAIN
+    apiKey: process.env.VITE_MAILGUN_API_KEY,
+    domain: process.env.VITE_MAILGUN_DOMAIN
   });
 
   const data = {
