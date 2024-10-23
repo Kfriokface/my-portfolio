0a8f7e8e248585777ddc04181b2f0f3725e9053c
Alberto Sancho <designalbertosancho@gmail.com>
2024-10-23 14:24:32 +0200

    config

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,4 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: './',
-  build: {
-    rollupOptions: {
-      output: {
-        format: 'es', // Asegúrate de que el formato es ESM
-      },
-    },
-  },
 })
