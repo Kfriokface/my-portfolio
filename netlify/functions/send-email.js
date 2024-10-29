cce19733618910397cf3f5a7e37de584b6dfda2c
Alberto Sancho <designalbertosancho@gmail.com>
2024-10-29 10:13:54 +0100

    config

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -1,6 +1,11 @@
-import { createTransport } from 'nodemailer';
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
 
-const transporter = createTransport({
+const app = express();
+app.use(bodyParser.json()); // Para procesar datos JSON
+
+const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.VITE_GOOGLE_DOMAIN,
@@ -8,8 +13,9 @@ const transporter = createTransport({
   },
 });
 
-export async function handler(event) {
-  const { email, message } = JSON.parse(event.body);
+// Ruta para recibir los datos del formulario
+app.post('/send-email', (req, res) => {
+  const { email, message } = req.body;
 
   const mailOptions = {
     from: email,
@@ -18,29 +24,42 @@ export async function handler(event) {
     text: message
   };
 
-  // const mailOptionsCopy = {
-  //   from: process.env.VITE_GOOGLE_DOMAIN,
-  //   to: email,
-  //   subject: "Copia de tu mensaje enviado a Alberto Sancho",
-  //   text: `Este es tu mensaje: \n\n${message}\n\nMe pondré en contacto contigo lo antes posible. Gracias.`
-  // };
+  const mailOptionsCopy = {
+    from: process.env.VITE_GOOGLE_DOMAIN,
+    to: email,
+    subject: "Copia de tu mensaje enviado a Alberto Sancho",
+    text: `Este es tu mensaje: \n\n${message}\n\nMe pondré en contacto contigo lo antes posible. Gracias.`
+  };
 
-  try {
-    await transporter.sendMail(mailOptions);
-    return {
-      statusCode: 200,
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ message: 'Correo enviado correctamente' }),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        error: 'Error al enviar el correo',
-        details: error.message
-      }),
-    };
-  }
-}
+  // Envía ambos correos
+  Promise.all([
+    transporter.sendMail(mailOptions),
+    transporter.sendMail(mailOptionsCopy)
+  ])
+  .then(() => {
+      res.status(200).send('Correos enviados con éxito');
+  })
+  .catch((error) => {
+      console.error('Error enviando correos:', error);
+      res.status(500).send('Error enviando correos');
+  });
+
+  // try {
+  //   await transporter.sendMail(mailOptions);
+  //   return {
+  //     statusCode: 200,
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     },
+  //     body: JSON.stringify({ message: 'Correo enviado correctamente' }),
+  //   };
+  // } catch (error) {
+  //   return {
+  //     statusCode: 500,
+  //     body: JSON.stringify({
+  //       error: 'Error al enviar el correo',
+  //       details: error.message
+  //     }),
+  //   };
+  // }
+});
