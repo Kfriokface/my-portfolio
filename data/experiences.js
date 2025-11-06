99c99078fc93ae1a135ef5df2675dadb0f9ed942
Kfriokface <designalbertosancho@gmail.com>
2025-11-06 11:19:04 +0100

    ajustes_contenido__061125

--- data/experiences.js
+++ data/experiences.js
@@ -1,6 +1,12 @@
 export const experiences = [
   {
     tab: "Full Stack Developer",
+    title: "Italfarmaco Spain",
+    description: "Desarrollo y mantenimiento de las webs de producto corporativas, realizadas tanto en Drupal como en Wordpress",
+    years: 'Octubre 2014 - actualidad',
+  },
+  {
+    tab: "Full Stack Developer",
     title: "Webconsultas.com",
     description: "Webconsultas es una revista de contenido digital realizada en Drupal. Realizo labores de desarrollo tanto en en backend como en el frontend, usando PHP, Javascript, CSS3 y HTML",
     years: 'Junio 2013 - actualidad',
--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -18,7 +18,7 @@ const Home = () => {
         <div class="about-me__text"> 
           <span>Hola, soy</span>
           <h1>Alberto Sancho</h1>
-          <h3>Desarrollador web y diseñador gráfico</h3>
+          <h3>Full stack web developer & Graphic designer</h3>
           <p>Soy un apasionado del desarrollo web, con experiencia en la creación de sitios web dinámicos y aplicaciones interactivas. Mi enfoque se centra en utilizar tecnologías modernas como HTML, CSS, JavaScript, y frameworks como React y Angular para construir experiencias web intuitivas y eficientes. Además, siempre estoy buscando nuevas herramientas y técnicas que optimicen el rendimiento y mejoren la experiencia del usuario.</p>
           <p>En el ámbito del diseño gráfico, cuento con una sólida experiencia en la creación de identidades visuales, diseño de logotipos, y diseño de interfaces. Mi enfoque visual se basa en la estética moderna, combinando funcionalidad con creatividad, asegurando que cada pieza visual no solo sea atractiva, sino también práctica y coherente con los objetivos del proyecto.</p>
           <p>Mi formación y práctica en ambas áreas me permiten crear soluciones integrales donde el diseño y la funcionalidad trabajan en conjunto. Esto me otorga una visión completa en la creación de productos digitales, desde el concepto inicial hasta su implementación final, logrando que el desarrollo y el diseño se complementen de manera fluida.</p>
